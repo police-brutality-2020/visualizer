8f4b1f428739881c78cf915054cb477c2fe1bf6b
Ryan Wirth <ryan@ryanwirth.ca>
2020-06-08 15:38:47 -0600

    Use the MapOptions interface instead

--- src/components/heat-map/HeatMap.js
+++ src/components/heat-map/HeatMap.js
@@ -7,19 +7,17 @@ const containerStyle = {
   height: '100vh',
 };
 
-const center = {
-  lat: 39.8097343,
-  lng: -98.5556199,
+const options = {
+  center: {
+    lat: 39,
+    lng: -98,
+  },
+  zoom: 5,
 };
 
 function HeatMap() {
-  const onLoad = React.useCallback((map) => {
-    map.setCenter(center);
-    map.setZoom(5);
-  }, []);
-
   return (
-    <GoogleMap onLoad={onLoad} mapContainerStyle={containerStyle}>
+    <GoogleMap mapContainerStyle={containerStyle} options={options}>
       <HeatmapLayer
         data={[
           new google.maps.LatLng(37.782, -122.447),
