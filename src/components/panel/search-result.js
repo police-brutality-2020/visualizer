43526bd01985189874c37c4ab54c1b601cebcae6
Ryan Wirth <ryan@ryanwirth.ca>
2020-06-09 10:57:38 -0600

    Pass result information as props

--- src/components/panel/search-result.js
+++ src/components/panel/search-result.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './search-result.css';
 
-function SearchResult() {
+function SearchResult({ title, location, date }) {
   return (
     <div className="search-result">
-      <h4>
-        Police shoot tear gas canister at man from close range, striking him in
-        the chest
-      </h4>
-      <p>Washington, D.C. &bull; June 9th, 2020</p>
+      <h4>{title}</h4>
+      <p>
+        {location} &bull; {date}
+      </p>
     </div>
   );
 }
+
+SearchResult.propTypes = {
+  title: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
 
 export default SearchResult;
--- src/components/panel/search-results.js
+++ src/components/panel/search-results.js
@@ -6,10 +6,26 @@ import './search-results.css';
 function SearchResults() {
   return (
     <div className="search-results">
-      <SearchResult />
-      <SearchResult />
-      <SearchResult />
-      <SearchResult />
+      <SearchResult
+        title="Police shoot tear gas canister at man from close range, striking him in the chest"
+        location="Washington, DC"
+        date="June 6th, 2020"
+      />
+      <SearchResult
+        title="Law enforcement shoot and throw tear gas at protesters"
+        location="Dallas, TX"
+        date="May 30th, 2020"
+      />
+      <SearchResult
+        title="Police shoot tear gas at peaceful protestors who have their hands up"
+        location="Atlanta, GA"
+        date="May 26th, 2020"
+      />
+      <SearchResult
+        title="Police fire tear gas and rubber bullets on peaceful assembly"
+        location="Minneapolis, MN"
+        date="May 25th, 2020"
+      />
     </div>
   );
 }
