ca5f3f13942bc775e1a057b3b9cff0567221178f
Kirk Yuan <yuanjt810@gmail.com>
2020-06-09 16:40:02 -0600

    Focuses to nearest city when clicking on map

--- src/util/closestCity.js
+++ src/util/closestCity.js
@@ -6,19 +6,21 @@ const distance = (coordinateA, coordinateB) => {
 
 const getClosetCity = (coordinate, results) => {
   const initialDistance = distance(coordinate, results[0].geocoding);
-  const closest = {
-    city: results[0],
+  const city = {
+    name: results[0],
     distance: initialDistance,
+    coordinate: results[0].geocoding,
   };
   for (let i = 0; i < results.length; i += 1) {
     const cityCoordinate = results[i].geocoding;
     const currentDistance = distance(coordinate, cityCoordinate);
-    if (currentDistance < closest.distance) {
-      closest.city = results[i].city;
-      closest.distance = currentDistance;
+    if (currentDistance < city.distance) {
+      city.name = results[i].city;
+      city.distance = currentDistance;
+      city.coordinate = cityCoordinate;
     }
   }
-  return closest;
+  return city;
 };
 
 export default getClosetCity;
