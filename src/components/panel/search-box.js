c53cafcf03cad3c5e5910404652210b02fd39ff9
Ryan Wirth <ryan@ryanwirth.ca>
2020-06-08 19:03:54 -0600

    Pass onSearch callback as a prop

--- src/components/panel/search-box.js
+++ src/components/panel/search-box.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './search-box.css';
 
-function SearchBox() {
+function SearchBox({ onSearch }) {
+  const [input, setInput] = React.useState('');
+
+  const handleChange = (e) => setInput(e.target.value);
+
+  const handleSubmit = (e) => {
+    onSearch(input);
+    e.preventDefault();
+  };
+
   return (
-    <div className="search-box">
-      <input type="text" placeholder="Search Police Brutality" />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Search Police Brutality"
+          value={input}
+          onChange={handleChange}
+        />
+      </div>
+    </form>
   );
 }
+
+SearchBox.propTypes = {
+  onSearch: PropTypes.func,
+};
+
+SearchBox.defaultProps = {
+  onSearch: () => {},
+};
 
 export default SearchBox;
