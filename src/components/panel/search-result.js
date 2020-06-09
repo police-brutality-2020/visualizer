5bc19aca067ae62b3acf1b90fbf8b73d84a9d8a9
Ryan Wirth <ryan@ryanwirth.ca>
2020-06-09 10:53:03 -0600

    Create a separate SearchResult component

---
+++ src/components/panel/search-result.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './search-result.css';
+
+function SearchResult() {
+  return (
+    <div className="search-result">
+      <h4>
+        Police shoot tear gas canister at man from close range, striking him in
+        the chest
+      </h4>
+      <p>Washington, D.C. &bull; June 9th, 2020</p>
+    </div>
+  );
+}
+
+export default SearchResult;
--- src/components/panel/search-results.js
+++ src/components/panel/search-results.js
@@ -1,31 +1,15 @@
 import React from 'react';
+import SearchResult from './search-result';
 
 import './search-results.css';
 
 function SearchResults() {
   return (
     <div className="search-results">
-      <div className="search-result">
-        <h4>
-          Police shoot tear gas canister at man from close range, striking him
-          in the chest
-        </h4>
-        <p>Washington, D.C. &bull; June 9th, 2020</p>
-      </div>
-      <div className="search-result">
-        <h4>
-          Police shoot tear gas canister at man from close range, striking him
-          in the chest
-        </h4>
-        <p>Washington, D.C. &bull; June 9th, 2020</p>
-      </div>
-      <div className="search-result">
-        <h4>
-          Police shoot tear gas canister at man from close range, striking him
-          in the chest
-        </h4>
-        <p>Washington, D.C. &bull; June 9th, 2020</p>
-      </div>
+      <SearchResult />
+      <SearchResult />
+      <SearchResult />
+      <SearchResult />
     </div>
   );
 }
