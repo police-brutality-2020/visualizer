286e5635bafe4bae89196b9774f81a971c6de76c
Ryan Wirth <ryan@ryanwirth.ca>
2020-06-08 18:39:49 -0600

    Create Panel component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Map from './components/map/map';
-import SearchBox from './components/search/search-box';
+import Panel from './components/panel/panel';
 
 import './App.css';
 
 function App() {
   return (
     <div>
-      <SearchBox />
+      <Panel />
       <Map />
     </div>
   );
---
+++ src/components/panel/panel.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SearchBox from './search-box';
+
+function Panel() {
+  return <SearchBox />;
+}
+
+export default Panel;
