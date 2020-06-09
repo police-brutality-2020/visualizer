9cf1e5e34c5b0a815357634cb4cf90b2379d3e58
Ryan Wirth <ryan@ryanwirth.ca>
2020-06-09 14:26:33 -0600

    Add ResultDetails actions

--- src/components/panel/result-details.js
+++ src/components/panel/result-details.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { FaTwitter, FaReddit } from 'react-icons/fa';
+
+import './result-details.css';
 
 function ResultDetails() {
-  return <div />;
+  return (
+    <div className="result-details">
+      <h3>
+        Police shoot tear gas canister at man from close range, striking him in
+        the chest
+      </h3>
+      <div className="actions">
+        <div>
+          <FaTwitter />
+          <span>@username1</span>
+        </div>
+        <div>
+          <FaReddit />
+          <span>u/username2</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ResultDetails;
