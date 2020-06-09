712b09999ba41f5e8744b1373f98ea95bf3849fa
Ryan Wirth <ryan@ryanwirth.ca>
2020-06-09 10:51:01 -0600

    Style search results

--- src/components/panel/search-results.js
+++ src/components/panel/search-results.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import './search-results.css';
+
 function SearchResults() {
   return (
     <div className="search-results">
-      <div>Hello World!</div>
-      <div>Hello World!</div>
+      <div className="search-result">
+        <h4>
+          Police shoot tear gas canister at man from close range, striking him
+          in the chest
+        </h4>
+        <p>Washington, D.C. &bull; June 9th, 2020</p>
+      </div>
+      <div className="search-result">
+        <h4>
+          Police shoot tear gas canister at man from close range, striking him
+          in the chest
+        </h4>
+        <p>Washington, D.C. &bull; June 9th, 2020</p>
+      </div>
+      <div className="search-result">
+        <h4>
+          Police shoot tear gas canister at man from close range, striking him
+          in the chest
+        </h4>
+        <p>Washington, D.C. &bull; June 9th, 2020</p>
+      </div>
     </div>
   );
 }
