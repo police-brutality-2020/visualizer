fcea7fc44bbf83860a897ebbe45a9b0940de5b55
Ryan Wirth <ryan@ryanwirth.ca>
2020-06-09 11:14:18 -0600

    Scroll search results when necessary

--- src/components/panel/search-results.js
+++ src/components/panel/search-results.js
@@ -4,30 +4,17 @@ import SearchResult from './search-result';
 import './search-results.css';
 
 function SearchResults() {
-  return (
-    <div className="search-results">
-      <SearchResult
-        title="Police shoot tear gas canister at man from close range, striking him in the chest"
-        location="Washington, DC"
-        date="June 6th, 2020"
-      />
-      <SearchResult
-        title="Law enforcement shoot and throw tear gas at protesters"
-        location="Dallas, TX"
-        date="May 30th, 2020"
-      />
-      <SearchResult
-        title="Police shoot tear gas at peaceful protestors who have their hands up"
-        location="Atlanta, GA"
-        date="May 26th, 2020"
-      />
-      <SearchResult
-        title="Police fire tear gas and rubber bullets on peaceful assembly"
-        location="Minneapolis, MN"
-        date="May 25th, 2020"
-      />
-    </div>
-  );
+  // TODO: Use real search results; this is here to force scrolling behavior
+  const results = [...Array(100).keys()].map((key) => (
+    <SearchResult
+      key={key}
+      title="Police shoot tear gas canister at man from close range, striking him in the chest"
+      location="Calgary, AB"
+      date="June 6th, 2020"
+    />
+  ));
+
+  return <div className="search-results">{results}</div>;
 }
 
 export default SearchResults;
